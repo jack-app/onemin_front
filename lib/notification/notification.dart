2250fef2acd0a6b6c4146ad6541bad7c92750616
potato482 <yakisobanotare@gmail.com>
2025-10-23 00:44:58 +0900

    Androidで通知送信をできるようにした

--- lib/notification/notification.dart
+++ lib/notification/notification.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 import 'package:flutter/services.dart';
+import 'package:onemin_front/services/simple_notification_service.dart';
 
 class NotificationTimerPage extends StatelessWidget{
+  
   final player = AudioPlayer();
 
   final audioSourceUrl = 'alarm.mp3';
@@ -16,12 +18,22 @@ class NotificationTimerPage extends StatelessWidget{
     await HapticFeedback.lightImpact();
   }
 
+  Future<void> initialize() async {
+    await SimpleNotificationService.initialize();
+  }
+
   void activateNotificationTimer({int minutes = 0, int seconds = 0}) {
     // ここに通知を設定するコードを追加
+    initialize();
     Timer(Duration(seconds: seconds, minutes: minutes), () {
       playSound();
       vibration();
       debugPrint("test");
+      SimpleNotificationService.showNotification(
+        id: 0,
+        title: 'Hello World !',
+        body: 'Happy Coding!',
+      );
     });
   }
 
