33848603912a795de91e2dec01c572fd0100c92c
Yosshi-I <yoshino0326cherry@gmail.com>
2025-11-13 02:38:20 +0900

    測定中画面の画面遷移

--- lib/measurement_running_screen.dart
+++ lib/measurement_running_screen.dart
@@ -1,45 +1,67 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'measurement_start_screen.dart';
 
+//計測スタート画面から時刻とタイトルの情報を持ってくる
 class MeasurementPage extends StatefulWidget {
+  final String selected_title;
+  final TimeOfDay selected_time_e;
+  final TimeOfDay selected_time_s;
+
+ void main(){
+    final now = DateTime.now();
+    final startTimeS = DateTime(now.year, now.month, now.day, selected_time_s.hour, selected_time_s.minute);
+    final startTimeE = DateTime(now.year, now.month, now.day, selected_time_e.hour, selected_time_e.minute);
+  }
+
+  const MeasurementPage({
+    super.key,
+    required this.selected_title,
+  });
 
     @override
     State<MeasurementPage> createState() => _MeasurementPageState();
 }
 
 class _MeasurementPageState extends State<MeasurementPage> { // ←クラス名は適当につけてます
-// （省略）他のプロパティの定義
+  //late;
+  //late;
 
-  String name = "通学準備";
-  Duration duration Duration(minutes: 5, seconds: 10);
+  //String selected_title = "通学準備";
+  //Duration duration Duration(minutes: 5, seconds: 10);
 
+  //@override
+  //void initState(){}
 
  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('計測中'),
+        title: Text(widget.selected_title),
         automaticallyImplyLeading: false, // 戻るボタンを非表示にする
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        children: Column(
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          children[
+          children:[
+            //時計の描画
+
             //ストップボタン
             ElevatedButton(
               onPressed: (){},
-              child: const Text('ストップ'),
               style: ElevatedButton.styleFrom(
-                backgroundcolor: Colors.red[300],
+                fixedSize: Size(200,200),
+                backgroundColor: Colors.red[300],
                 foregroundColor: Colors.black,
               ),
+              child: const Text('ストップ'),
             ),
           ],
         ),
       ),
-    ),
-  },
-},
+    );
+  }
+}
 
