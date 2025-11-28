fc7a1fafd360ce7a8b5130a97f7b27a5531f1a1b
wamikan <wamikan@icloud.com>
2025-11-28 18:02:02 +0900

    feat_#12

--- lib/measurement_start_screen.dart
+++ lib/measurement_start_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'schedule_creation_screen.dart';
 import 'main.dart'; // Scheduleクラスをインポート
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MeasurementStartScreen extends StatefulWidget {
   const MeasurementStartScreen({super.key});
@@ -10,7 +15,25 @@ class MeasurementStartScreen extends StatefulWidget {
 }
 
 class _MeasurementStartScreenState extends State<MeasurementStartScreen> {
-  final List<Schedule> _schedules = [];
+  List<Schedule> _schedules = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSchedules();
+  }
+
+  Future<void> _loadSchedules() async {
+    final prefs = await SharedPreferences.getInstance();
+    final stored = prefs.getStringList('schedules') ?? [];
+    final loaded = stored.map((encoded) {
+      final data = jsonDecode(encoded) as Map<String, dynamic>;
+      return _scheduleFromMap(data);
+    }).toList();
+    setState(() {
+      _schedules = loaded;
+    });
+  }
 
   // スケジュール作成画面に遷移し、新しいスケジュールを受け取るメソッド
   void _navigateAndAddSchedule(BuildContext context) async {
@@ -21,17 +44,61 @@ class _MeasurementStartScreenState extends State<MeasurementStartScreen> {
     );
 
     // データが返ってきた場合、Scheduleオブジェクトを生成してリストに追加
+    //if (result != null) {
+    //final newSchedule = Schedule(
+    //    title: result['title'],
+    //    startTime: result['startTime'],
+    //    endTime: result['endTime'],
+    //  );
+    //  setState(() {
+    //    _schedules.add(newSchedule);
+    //  });
+    //}
+    
     if (result != null) {
+      final prefs = await SharedPreferences.getInstance();
       final newSchedule = Schedule(
-        title: result['title'],
-        startTime: result['startTime'],
-        endTime: result['endTime'],
+        title: result['title'] as String,
+        startTime: result['startTime'] as TimeOfDay,
+        endTime: result['endTime'] as TimeOfDay,
       );
+
+      final stored = prefs.getStringList('schedules') ?? [];
+      stored.add(jsonEncode(_scheduleToMap(newSchedule)));
+      await prefs.setStringList('schedules', stored);
+
       setState(() {
-        _schedules.add(newSchedule);
+        _schedules = [..._schedules, newSchedule];
       });
     }
   }
+
+  Map<String, dynamic> _scheduleToMap(Schedule schedule) {
+    return {
+      'title': schedule.title,
+      'startHour': schedule.startTime.hour,
+      'startMinute': schedule.startTime.minute,
+      'endHour': schedule.endTime.hour,
+      'endMinute': schedule.endTime.minute,
+    };
+  }
+
+  Schedule _scheduleFromMap(Map<String, dynamic> data) {
+    final startHour = data['startHour'] as int;
+    final startMinute = data['startMinute'] as int;
+    final endHour = data['endHour'] as int;
+    final endMinute = data['endMinute'] as int;
+    return Schedule(
+      title: data['title'] as String,
+      startTime: TimeOfDay(hour: startHour, minute: startMinute),
+      endTime: TimeOfDay(hour: endHour, minute: endMinute),
+    );
+  }
+  
+  
+  
+
+
 
   @override
   Widget build(BuildContext context) {
